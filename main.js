a4227a57e822b819f8d58590e10df06807769a46
Masha <valzyr48@gmail.com>
2019-11-06 17:51:47 +0300

    feat: add methods for button

--- main.js
+++ main.js
@@ -6,4 +6,19 @@ class Button {
       this.type = obj.type;
       this.button = this.createEl();
     }
+    createEl = () => {
+        const el = document.createElement('div');
+        el.innerText = this.first;
+        el.id = this.id;
+        el.classList.add(this.type, 'button', 'up');
+        if (this.first === 'Space') {
+          el.classList.add('space');
+          el.innerText = '';
+        }
+        return el;
+      };
+    
+      render = () => {
+        return this.button;
+      };
   }
